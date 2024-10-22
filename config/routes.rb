663bf4b1cee37cbed4d7754b9b6a20f59bb91583
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-10-22 17:18:01 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
   # Defines the root path route ("/")
   # root "posts#index"
 end
